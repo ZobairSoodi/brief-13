872d4239614078072a43ec6a7937ebaa9b9abc29
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-02-24 12:40:07 +0100

    Update sqlFile.sql

--- sqlFile.sql
+++ sqlFile.sql
@@ -10,16 +10,43 @@ CREATE TABLE Client(
     email VARCHAR(50),
     password VARCHAR(25)
 );
+INSERT INTO Client VALUES
+(1,'fn1','ln1','adr1','tel1','em1','ps1'),
+(2,'fn2','ln2','adr2','tel2','em2','ps2'),
+(3,'fn3','ln3','adr3','tel3','em3','ps3');
+
 CREATE TABLE Product(
 	id_product INT PRIMARY KEY,
     label VARCHAR(25),
     description VARCHAR(50),
     price FLOAT,
-    quantity INT
+    quantity_in_stock INT
 );
+INSERT INTO Product VALUES
+(1,'lb1','desc1',43,12),
+(2,'lb2','desc2',5,54),
+(3,'lb3','desc3',123,43);
+
 CREATE TABLE Order_(
 	id_order INT PRIMARY KEY,
-    quantity_ordered INT,
     date_ordered DATE,
-    shipping_adress VARCHAR(50)
+    shipping_adress VARCHAR(50),
+    id_client INT REFERENCES Client(id_client)
 );
+INSERT INTO Order_ VALUES
+(1,'2013-06-12','ad1',1),
+(2,'2014-06-01','ad2',2),
+(3,'2005-11-09','ad3',3);
+
+CREATE TABLE Order_Product_Details(
+	quantity_ INT,
+    id_product INT REFERENCES Product(id_product),
+    id_order INT REFERENCES Order_(id_order),
+    PRIMARY KEY(id_product,id_order)
+);
+INSERT INTO Order_Product_Details VALUES
+(12,1,1),
+(10,2,1),
+(12,1,2),
+(1,3,1);
+
